04c025997504bb8fe080564430e8bd13db365607
Olly Percival <ollypercival@gmail.com>
2025-12-30 21:08:54 +0000

    Trying to fix build issues

--- astro.config.ts
+++ astro.config.ts
@@ -15,6 +15,7 @@ import { SITE } from "./src/config";
 export default defineConfig({
   site: SITE.website,
   base: "/raxplays",
+  trailingSlash: 'always',
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
