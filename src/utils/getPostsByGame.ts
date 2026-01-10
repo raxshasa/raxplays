6544da615b1612fb5a815a51bcfc610c32910d3e
Olly Percival <ollypercival@gmail.com>
2026-01-10 22:33:01 +0000

    Some final changes

--- src/utils/getPostsByGame.ts
+++ src/utils/getPostsByGame.ts
@@ -2,9 +2,13 @@ import type { CollectionEntry } from "astro:content";
 import getSortedPosts from "./getSortedPosts";
 import { slugifyAll } from "./slugify";
 
-const getPostsByGame = (posts: CollectionEntry<"blog">[], tag: string) =>
+// We changed '_tag' to 'game' so it matches the usage inside .includes()
+const getPostsByGame = (posts: CollectionEntry<"blog">[], game: string) =>
   getSortedPosts(
-    posts.filter(post => slugifyAll(post.data.games).includes(game))
+    posts.filter(post => 
+      // Ensure post.data.games exists before trying to slugify it
+      post.data.games && slugifyAll(post.data.games).includes(game)
+    )
   );
 
 export default getPostsByGame;
